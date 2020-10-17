31f97c0d527b0eda2073f248303155b253571ede
Andrey Leontyev <andruwwwka@gmail.com>
2020-10-17 19:50:06 +0000

    Добавлен эндпоинт для задач без привязки к целям

--- education/resources/task.py
+++ education/resources/task.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Task
 
@@ -16,3 +18,13 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(methods=['get'], detail=False)
+    def orphans(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().filter(level__isnull=True))
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)

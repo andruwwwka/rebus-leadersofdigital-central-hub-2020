1b6e3f18fdf0f3ce279b0ae134cec309da64a253
konovalov <ikonovalov@7tech.ru>
2020-10-17 09:41:50 +0300

    feature/pagination add pagination for ban_content

--- sber/settings.py
+++ sber/settings.py
@@ -146,7 +146,9 @@ REST_FRAMEWORK = {
     #     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'users.backends.JWTAuthentication',
-        )
+        ),
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 100
 }
 
 

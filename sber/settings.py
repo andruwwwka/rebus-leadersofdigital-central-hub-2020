c3ae43d0f5d1ae9e20b15f5851345f3ac436f279
konovalov <ikonovalov@7tech.ru>
2020-10-17 13:51:53 +0300

    rename add task for Level (target)

--- sber/settings.py
+++ sber/settings.py
@@ -147,8 +147,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'users.backends.JWTAuthentication',
         ),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 100
 }
 
 
